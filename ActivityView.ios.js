f0c565fedc3f758678aa45184e49d181a9ce72d9
hosoi <hosoi@appland.co.jp>
2016-05-24 19:33:18 +0900

    fix: RN0.26

--- ActivityView.ios.js
+++ ActivityView.ios.js
@@ -4,19 +4,7 @@
  */
 'use strict';
 
-var React = require('react-native');
-var {
-  NativeModules
-} = React;
-
-var NativeActivityView = NativeModules.ActivityView;
-
-/**
- * High-level docs for the ActivityView iOS API can be written here.
- */
-
-var ActivityView = {
-  show: NativeActivityView.show
+const NativeActivityView = require('react-native').NativeModules.ActivityView;
+module.exports = {
+  show: NativeActivityView.show,
 };
-
-module.exports = ActivityView;
