naoufal/react-native-activity-view

commit 983f7c70bd65503479de4a82c21bd9496b369998

Added support for sharing a file. Added file support to readme Added support for use while modal is open (from issue https://github.com/naoufal/react-native-activity-view/issues/31)

ActivityView.m
@@ -56,6 +56,7 @@ RCT_EXPORT_METHOD(show:(NSDictionary *)args)
     NSArray *activitiesToExclude = args[@"exclude"];
     NSString *image = args[@"image"];
     NSString *imageBase64 = args[@"imageBase64"];
+    NSObject *file = args[@"file"];
     NSData *imageData;
     
     // Try to fetch image
@@ -88,7 +89,11 @@ RCT_EXPORT_METHOD(show:(NSDictionary *)args)
     if (url) {
         [shareObject addObject:url];
     }
-    
+
+    if (file) {
+        [shareObject addObject:file];
+    }
+
     if (image) {
         [shareObject addObject: [UIImage imageNamed: image]];
     } else if (imageData) {
@@ -122,7 +127,7 @@ RCT_EXPORT_METHOD(show:(NSDictionary *)args)
             activityView.popoverPresentationController.permittedArrowDirections = 0;
         }
     }
-    [ctrl presentViewController:activityView animated:YES completion:nil];
+    [ctrl.presentedViewController presentViewController:activityView animated:YES completion:nil];
 }
 
 @end

commit e44c6e0b4a5568aa98f5e9776d7481cb7d4d88e5

Fix regression causing view to fail to appear
Introduced in https://github.com/naoufal/react-native-activity-view/commit/983f7c70bd65503479de4a82c21bd9496b369998#diff-6fabc94cc5932c6337aa5e24bc89c7deL125

Fixes #47

ActivityView.m
@@ -153,7 +153,7 @@ RCT_EXPORT_METHOD(show:(NSDictionary *)args)
             activityView.popoverPresentationController.permittedArrowDirections = 0;
         }
     }
-    [ctrl.presentedViewController presentViewController:activityView animated:YES completion:nil];
+    [ctrl presentViewController:activityView animated:YES completion:nil];
 }
 
 @end